b981b381623dd3f143bc578a52d84f1174c921d2
Scott Yannitell <scott@ohiovr.com>
2017-11-04 09:47:40 -0400

    5

--- 3. make virtualbox shared folders accessable to nextcloud and you.sh
+++ 3. make virtualbox shared folders accessable to nextcloud and you.sh
@@ -1,6 +1,8 @@
 #make vboxsf a member of the group of www-data
 #allow myself acces to my own files than you very much
-sudo usermod -a -G vboxsf $(whoami)
+echo "type in the user name you wish to add to the virtualbox shared folders access group"
+read userName
+sudo usermod -a -G vboxsf $userName
 #allow the webserver acces to the files thank you very much
 sudo usermod -a -G vboxsf www-data
 clear
