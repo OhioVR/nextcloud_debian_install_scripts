627b1a822041e1840de0c15d4bfb43cb0e014d43
Scott Yannitell <scott@ohiovr.com>
2017-11-05 10:00:25 -0500

    t

--- 7. install SOLR and nextant for full text search.sh
+++ 7. install SOLR and nextant for full text search.sh
@@ -26,11 +26,13 @@ echo $NEXTANT_VERSION
 
 
 echo "Starting to setup Solr & Nextant on Nextcloud..."
-
+clear
+echo "1"
 # Installing requirements
 apt update -q4 & spinner_loading
 apt install default-jre -y
-
+clear
+echo "2"
 # Getting and installing Apache Solr
 echo "Installing Apache Solr"
 echo "It might take some time depending on your bandwith, please be patient..."
@@ -46,16 +48,15 @@ else
     echo "Solr failed to install, something is wrong with the Solr installation"
     exit 1
 fi
-
+clear
+echo "3"
 sudo sed -i '35,37  s/"jetty.host" \//"jetty.host" default="127.0.0.1" \//' $SOLR_JETTY
 
 iptables -A INPUT -p tcp -s localhost --dport 8983 -j ACCEPT
 iptables -A INPUT -p tcp --dport 8983 -j DROP
-# Not tested
-#sudo apt install iptables-persistent
-#sudo service iptables-persistent start
-#sudo iptables-save > /etc/iptables.conf
 
+clear
+echo "4"
 if bash | service solr start
 then
     sudo -u solr /opt/solr/bin/solr create -c nextant
@@ -63,7 +64,8 @@ else
     echo "Solr failed to start, something is wrong with the Solr installation"
     exit 1
 fi
-
+clear
+echo "4"
 # Add search suggestions feature
 sed -i '2i <!DOCTYPE config [' "$SOLR_DSCONF"
 sed -i "3i   <\!ENTITY nextant_component SYSTEM \"$NCPATH/apps/nextant/config/nextant_solrconfig.xml\"\>" "$SOLR_DSCONF"
@@ -73,23 +75,28 @@ sed -i '$d' "$SOLR_DSCONF" | sed -i '$d' "$SOLR_DSCONF"
 echo "
 &nextant_component;
 </config>" | tee -a "$SOLR_DSCONF"
-
+clear
+echo "5"
 check_command "echo \"SOLR_OPTS=\\\"\\\$SOLR_OPTS -Dsolr.allow.unsafe.resourceloading=true\\\"\" | sudo tee -a /etc/default/solr.in.sh"
-
+clear
+echo "6"
 check_command service solr restart
-
+clear
+echo "7"
 # Get nextant app for nextcloud
 check_command wget -q -P "$NC_APPS_PATH" "$NT_DL"
 check_command cd "$NC_APPS_PATH"
 check_command tar zxf "$NT_RELEASE"
-
+clear
+echo "8"
 # Enable Nextant
 rm -r "$NT_RELEASE"
 check_command sudo -u www-data php $NCPATH/occ app:enable nextant
 chown -R www-data:www-data $NCPATH/apps
 check_command sudo -u www-data php $NCPATH/occ nextant:test http://127.0.0.1:8983/solr/ nextant --save
 check_command sudo -u www-data php $NCPATH/occ nextant:index
-
+clear
+echo "9"
 
 
 
