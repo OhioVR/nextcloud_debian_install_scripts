78a0814b525ba87295ec1229374f1f2f6b33fb94
Scott Yannitell <scott@ohiovr.com>
2017-11-05 05:45:26 -0500

    optimize

--- 6. optimize nextcloud a little.sh
+++ 6. optimize nextcloud a little.sh
@@ -1,4 +1,4 @@
 #optimize nextcloud
 echo "there is an error message in nextcloud's admin panel saying that opcache is all broke and stuff"
 echo "so this script will copy a more fleshed out opcache.ini file to where it needs to go"
-cp optcache.ini /etc/php/7.0/mods-available/opcache.ini
+cp opcache.ini /etc/php/7.0/mods-available/opcache.ini
