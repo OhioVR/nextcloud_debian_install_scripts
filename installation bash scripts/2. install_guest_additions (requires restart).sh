772e4d6fed26f8e0945a98e9c1a7cbc16683a06e
Scott Yannitell <scott@ohiovr.com>
2017-11-07 04:49:49 -0500

    refinement to virtualbox guest additions setup

--- installation bash scripts/2. install_guest_additions (requires restart).sh
+++ installation bash scripts/2. install_guest_additions (requires restart).sh
@@ -2,7 +2,10 @@ apt-get update
 apt-get upgrade
 apt-get install build-essential module-assistant
 m-a prepare
+clear
 read -p "insert guest additions then press enter"
+echo "sleeping for ten seconds..."
+sleep 10
 mount /dev/sr0 /media/cdrom
 sh /media/cdrom/VBoxLinuxAdditions.run
 clear
--- maintenance scripts/force nextant to reindex immediately.sh
+++ maintenance scripts/force nextant to reindex immediately.sh
@@ -1,2 +1,3 @@
 cd /var/www/vhosts/nextcloud/
 sudo -u www-data php occ nextant:index
+sudo -u www-data php occ nextant:index --bookmarks
